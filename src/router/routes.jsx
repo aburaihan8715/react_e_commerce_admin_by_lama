619b188310dcd0c5df8b2cf3c6a02672f2a3bfd4
aburahan8721 <aburaihan8721@gmail.com>
2024-01-10 12:20:15 +0600

    ui done

---
+++ src/router/routes.jsx
@@ -0,0 +1,47 @@
+import { createBrowserRouter } from "react-router-dom";
+import AppLayout from "../layouts/AppLayout";
+import Error from "../pages/error/Error";
+import Home from "../pages/home/Home";
+import UserList from "../pages/userList/UserList";
+import User from "../pages/user/User";
+import NewUser from "../pages/newUser/NewUser";
+import ProductList from "../pages/productList/ProductList";
+import Product from "../pages/product/Product";
+import NewProduct from "../pages/newProduct/NewProduct";
+
+export const router = createBrowserRouter([
+  {
+    element: <AppLayout />,
+    errorElement: <Error />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/users",
+        element: <UserList />,
+      },
+      {
+        path: "/user/:userId",
+        element: <User />,
+      },
+      {
+        path: "/newUser",
+        element: <NewUser />,
+      },
+      {
+        path: "/products",
+        element: <ProductList />,
+      },
+      {
+        path: "/product/:productId",
+        element: <Product />,
+      },
+      {
+        path: "/newproduct",
+        element: <NewProduct />,
+      },
+    ],
+  },
+]);
